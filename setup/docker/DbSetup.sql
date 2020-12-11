8efefbbea7d9724806614b30c62e65d880d58593
Pratibha Patil <pratibha.patil@siemens.com>
2020-12-11 17:43:25 +0530

    Finalized web app code

--- setup/docker/DbSetup.sql
+++ setup/docker/DbSetup.sql
@@ -1,6 +1,6 @@
 USE testdb;
 CREATE TABLE appmsg (id INT NOT NULL, message VARCHAR(200) NULL, PRIMARY KEY (id));
 INSERT INTO appmsg (id, message) VALUES (1, 'Hello Team');
-CREATE USER 'testUsr'@'localhost' IDENTIFIED WITH mysql_native_password BY 'wournEwyin';
-GRANT ALL ON testdb.* TO 'testUsr'@'localhost';
+CREATE USER 'testUsr'@'%' IDENTIFIED WITH mysql_native_password BY 'wournEwyin';
+GRANT ALL ON *.* TO 'testUsr'@'%';
 FLUSH PRIVILEGES;
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,36 @@
 import express from 'express';
-import {conn} from './connectDB';
+import mysql from 'mysql';
+import * as fs from 'fs';
 
 const app = express();
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 
 app.get('/message', (req, res)=>{
-    conn.query('select message from appmsg', (err, rows, fields)=>{
-        if(!err){
-            res.send(rows);
-        } else {
-            console.log(err);
+    fs.readFile('/vault/secrets/database-config.txt', 'utf8' , (err, data) => {
+    //fs.readFile('./test.txt', 'utf8' , (err, data) => {  //Use this for application run on local system with local mysql db
+      if (err) {
+          console.error(err)
+          return
         }
-    })
-})
+        const fdata = data;
+
+        const url = fdata.split('#').pop();
+ 
+        const conn = mysql.createConnection(url);
+
+        console.log(`mysql env var ${url}`);
+
+        conn.query('select message from appmsg', (err, rows, fields)=>{
+          if(!err){
+              console.log('Mysql DB connection is successful..sending data on UI');
+              res.send(rows);
+          } else {
+              console.log(err);
+          }
+        });  
+    });
+});
 
 app.listen({port: port}, () => {
     return console.log(`Server running on http://${host}:${port}`);
