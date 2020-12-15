ffec57244ea9be62fb9cb12e9bd01c93da8346bc
Pratibha-28 <patil.pratibha28@gmail.com>
2020-12-15 09:26:27 +0530

    Updated setup file as per latest docker image 5.7

--- setup/docker/DbSetup.sql
+++ setup/docker/DbSetup.sql
@@ -1,6 +1,6 @@
 USE testdb;
 CREATE TABLE appmsg (id INT NOT NULL, message VARCHAR(200) NULL, PRIMARY KEY (id));
 INSERT INTO appmsg (id, message) VALUES (1, 'Hello Team');
-CREATE USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL ON *.* TO 'user'@'%';
+CREATE USER 'testUsr'@'%' IDENTIFIED WITH mysql_native_password BY 'wournEwyin';
+GRANT ALL ON *.* TO 'testUsr'@'%';
 FLUSH PRIVILEGES;
